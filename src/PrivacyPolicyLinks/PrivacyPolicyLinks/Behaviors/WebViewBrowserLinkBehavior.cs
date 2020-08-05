fdcd3ece7a791d01b2b06b0f10c78221ef843a6b
AdenEarnshaw <github@adenearnshaw.com>
2020-08-05 22:36:59 +0100

    Adding Sample App

---
+++ src/PrivacyPolicyLinks/PrivacyPolicyLinks/Behaviors/WebViewBrowserLinkBehavior.cs
@@ -0,0 +1,65 @@
+﻿using Behaviors;
+using System;
+using Xamarin.Essentials;
+using Xamarin.Forms;
+
+namespace PrivacyPolicyLinks.Behaviors
+{
+	public class WebViewBrowserLinkBehavior : BehaviorBase<WebView>
+	{
+		public BrowserLaunchOptions LaunchOptions { get; set; } = new BrowserLaunchOptions
+		{
+			LaunchMode = BrowserLaunchMode.SystemPreferred,
+			TitleMode = BrowserTitleMode.Default,
+			PreferredControlColor = Color.FromHex("#f3f3f3"),
+			PreferredToolbarColor = Color.FromHex("#212121")
+		};
+
+		public BrowserLaunchMode LaunchMode { get => LaunchOptions.LaunchMode; set => LaunchOptions.LaunchMode = value; }
+		public BrowserTitleMode TitleMode { get => LaunchOptions.TitleMode; set => LaunchOptions.TitleMode = value; }
+
+		public static readonly BindableProperty ToolbarBackgroundColorProperty
+			= BindableProperty.Create(nameof(ToolbarBackgroundColor), typeof(Color), typeof(WebViewBrowserLinkBehavior), propertyChanged: UpdateBackgroundColor);
+		public static readonly BindableProperty ToolbarForegroundColorProperty
+			= BindableProperty.Create(nameof(ToolbarForegroundColor), typeof(Color), typeof(WebViewBrowserLinkBehavior), propertyChanged: UpdateForegroundColor);
+
+		public Color ToolbarBackgroundColor
+		{
+			get { return (Color)GetValue(ToolbarBackgroundColorProperty); }
+			set { SetValue(ToolbarBackgroundColorProperty, value); }
+		}
+		public Color ToolbarForegroundColor
+		{
+			get { return (Color)GetValue(ToolbarForegroundColorProperty); }
+			set { SetValue(ToolbarForegroundColorProperty, value); }
+		}
+
+
+		protected override void OnAttachedTo(WebView webView)
+		{
+			base.OnAttachedTo(webView);
+			webView.Navigating += WebViewOnNavigating;
+		}
+
+		protected override void OnDetachingFrom(WebView webView)
+		{
+			base.OnDetachingFrom(webView);
+			webView.Navigating -= WebViewOnNavigating;
+		}
+
+		private void WebViewOnNavigating(object sender, WebNavigatingEventArgs e)
+		{
+			if (!e.Url.StartsWith("http", StringComparison.InvariantCultureIgnoreCase))
+				return;
+
+			e.Cancel = true;
+			_ = Browser.OpenAsync(e.Url, this.LaunchOptions);
+		}
+
+		private static BindableProperty.BindingPropertyChangedDelegate UpdateBackgroundColor = (b, o, n) =>
+			 ((WebViewBrowserLinkBehavior)b).LaunchOptions.PreferredToolbarColor = (Color)n;
+
+		private static BindableProperty.BindingPropertyChangedDelegate UpdateForegroundColor = (b, o, n) =>
+			((WebViewBrowserLinkBehavior)b).LaunchOptions.PreferredControlColor = (Color)n;
+	}
+}
---
+++ src/PrivacyPolicyLinks/PrivacyPolicyLinks/MainPage.xaml.cs
@@ -0,0 +1,34 @@
+﻿using System.ComponentModel;
+using System.IO;
+using System.Reflection;
+using Xamarin.Forms;
+
+namespace PrivacyPolicyLinks
+{
+    [DesignTimeVisible(false)]
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+            BindingContext = new MainPageViewmodel();
+        }
+    }
+
+    public class MainPageViewmodel
+    {
+        public string DocumentText { get; }
+
+        public MainPageViewmodel()
+        {
+            var assembly = Assembly.GetExecutingAssembly();
+            var resourceName =$"{nameof(PrivacyPolicyLinks)}.Resources.Index.html";
+
+            using (Stream stream = assembly.GetManifestResourceStream(resourceName))
+            using (StreamReader reader = new StreamReader(stream))
+            {
+                DocumentText = reader.ReadToEnd();
+            }
+        }
+    }
+}
